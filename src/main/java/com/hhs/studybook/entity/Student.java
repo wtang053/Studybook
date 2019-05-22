0dcc7edcd1744364aa10e0857fe1e45d27c00200
Junhe Cui <jcui491@student.fuhsd.org>
2019-05-22 09:21:12 -0700

    Updated constructor. Added columns and getters and setters

--- src/main/java/com/hhs/studybook/entity/Student.java
+++ src/main/java/com/hhs/studybook/entity/Student.java
@@ -57,6 +57,7 @@ public class Student implements Serializable, Comparable {
     public Student() {
     }
 
+
     /**
      * A student object with name, gender, and age stored.
      *
@@ -210,6 +211,62 @@ public class Student implements Serializable, Comparable {
                 ", gender='" + gender + '\'' +
                 ", age=" + age +
                 '}';
+    }
+
+    public String getClass1() {
+        return class1;
+    }
+
+    public void setClass1(String class1) {
+        this.class1 = class1;
+    }
+
+    public String getClass2() {
+        return class2;
+    }
+
+    public void setClass2(String class2) {
+        this.class2 = class2;
+    }
+
+    public String getClass3() {
+        return class3;
+    }
+
+    public void setClass3(String class3) {
+        this.class3 = class3;
+    }
+
+    public String getClass4() {
+        return class4;
+    }
+
+    public void setClass4(String class4) {
+        this.class4 = class4;
+    }
+
+    public String getClass5() {
+        return class5;
+    }
+
+    public void setClass5(String class5) {
+        this.class5 = class5;
+    }
+
+    public String getClass6() {
+        return class6;
+    }
+
+    public void setClass6(String class6) {
+        this.class6 = class6;
+    }
+
+    public String getClass7() {
+        return class7;
+    }
+
+    public void setClass7(String class7) {
+        this.class7 = class7;
     }
 
     /**
